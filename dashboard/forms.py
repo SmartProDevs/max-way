bba3451d49601c2515b1844d01de32e8ab2a02a6
SmartProDevs <smartprodevs@gmail.com>
2021-07-16 16:48:13 +0300

    add news

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,6 +5,10 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model=Category
         fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
 
 class ProductForm(forms.ModelForm):
     model = Product
