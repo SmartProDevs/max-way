86ae1e57be669a791b1d88cf91018f6587fa36a7
SmartProDevs <smartprodevs@gmail.com>
2021-07-21 16:35:48 +0300

    add to cart

--- food/views.py
+++ food/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+from .services import *
 from config.settings import MEDIA_ROOT
+
+def home_page(request):
+    if request.GET:
+        product = get_product_by_id(request.GET.get("product_id", 0))
+        return JsonResponse(product)
+
 def index(request):
     categories = Category.objects.all()
     products = Product.objects.all()
-    print(categories)
-    print(products)
+
     ctx={
         'categories':categories,
         'products': products,
